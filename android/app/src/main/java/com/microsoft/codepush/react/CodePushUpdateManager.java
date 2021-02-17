c7aff7b36f39388ef6f80a9c5cf646b93470e912
TeaNose <agustinuswikrama@gmail.com>
2021-02-17 11:53:46 +0700

    remove import java.net.HttpsUrlConnection

--- android/app/src/main/java/com/microsoft/codepush/react/CodePushUpdateManager.java
+++ android/app/src/main/java/com/microsoft/codepush/react/CodePushUpdateManager.java
@@ -9,7 +9,6 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.HttpsURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.ByteBuffer;
